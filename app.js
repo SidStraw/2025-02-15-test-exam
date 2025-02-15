1e31a150367dccccfb9d6adbda54f7678dfc91d2
SidStraw <j80304xz@gmail.com>
2025-02-15 14:36:56 +0800

    done

--- app.js
+++ app.js
@@ -1,32 +1,32 @@
 function App() {
+  const [data, setData] = React.useState([])
+
+  React.useEffect(async () => {
+    try {
+      const res = await axios.get('https://randomuser.me/api/?results=10')
+      setData(res.data.results)
+    } catch (error) {
+      alert('錯誤', error.message)
+    }
+  },[])
+
   return (
     <div className="container mx-auto p-4">
       <div className="row">
-        <div className="col-md-4">
-          <div className="bg-light p-3">
-            <img
-              src="https://randomuser.me/api/portraits/women/61.jpg"
-              alt="頭像"
-              className="img-fluid rounded-circle"
-            />
-            <h2 className="mb-0">Mona Heen</h2>
-            <p className="mb-0">mona.heen@example.com</p>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="bg-light p-3">
-            <img
-              src="https://randomuser.me/api/portraits/women/10.jpg"
-              alt="頭像"
-              className="img-fluid rounded-circle"
-            />
-            <h2 className="mb-0">Susan Craig</h2>
-            <p className="mb-0">susan.craig@example.com</p>
-          </div>
-        </div>
+        {data.map(item => {
+          return (
+            <div className="col-md-4">
+              <div className="bg-light p-3">
+                <img src={item.picture.medium} alt="頭像" className="img-fluid rounded-circle" />
+                <h2 className="mb-0">{item.name.first} {item.name.last}</h2>
+                <p className="mb-0">{item.email}</p>
+              </div>
+            </div>
+          )
+        })}
       </div>
     </div>
-  );
+  )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
